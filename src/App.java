5ac17686cb91329380baf7de1d3756a632307317
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-30 14:07:42 +0200

    featMain/App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.util.*;
+import net.salesianos.Reparaciones.Reparacion;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Queue<Reparacion> cola = new LinkedList<>();
+
+        cola.add(new Reparacion("R001", "Ana", "Cambio pantalla", false));
+        cola.add(new Reparacion("R002", "Luis", "Bateria", true));
+        cola.add(new Reparacion("R003", "Marta", "Teclado", false));
+        cola.add(new Reparacion("R004", "Pedro", "Puerto", true));
+
+        System.out.println("Pendientes:");
+        for (Reparacion r : cola) {
+            System.out.println(r);
+        }
+
+        System.out.println("Primera: " + cola.peek());
+
+        Stack<Reparacion> pila = new Stack<>();
+
+        pila.push(cola.poll());
+        pila.push(cola.poll());
+
+        System.out.println("Ultima atendida: " + pila.peek());
+
+        pila.pop();
+
+        System.out.println("Pendientes: " + cola.size());
+        System.out.println("¿Cola vacia? " + cola.isEmpty());
     }
 }
